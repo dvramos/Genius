e5967ce87d1ddff5b453865ff88c9cefedd4741c
dvramos <dvramos@hotmail.com.br>
2020-10-02 08:28:25 -0300

    5/6 DIO

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let shuffleOrder = () => {
     clickedOrder = [];
 
     for(let i in order){
-        let elementColor = createElement(order[i]);
+        let elementColor = createColorElement(order[i]);
         lightColor(elementColor, Number(i) + 1);
     }
 }
@@ -36,17 +36,17 @@ let lightColor = (element, number) => {
 
 }
 
-//checase os botões clicados são os mesmos da ordem gerada
+//checa se os botões clicados são os mesmos da ordem gerada no jogo
 let checkOrder = () =>{
     for(let i in clickedOrder) {
         if(clickedOrder[i] != order[i]){
-            lose();
+            gameOver();
             break;
         }
     }
 
 
-    for(clickedOrder.length == order.length){
+    if(clickedOrder.length == order.length){
         alert(`Pontuação: ${score}\n Você acertou! Iniciando próximo nível!`);
         nextLevel();
     }
@@ -55,11 +55,54 @@ let checkOrder = () =>{
 //função para o clique do usuario
 let click = (color) => {
     clickedOrder[clickedOrder.length] = color;
-    elementColor(color).classList.add('selected');
+    createColorElement(color).classList.add('selected');
 
     setTimeout(() =>{
-        elementColor(color).classList.remove('selected');
-    })
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    }, 250);
     
-    checkOrder();
+  
 }
+
+//função que retorna a cor
+let createColorElement = (color) => {
+    if(color == 0){
+        return green;
+     } else if (color == 1) {
+        return red;
+     } else if (color == 2) {
+        return yellow;
+     } else if (color == 3) {
+        return blue;
+     }
+}
+
+//função para o próximo nível do jogo
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+//função para Game Over
+let gameOver = () => {
+    alert(`Pontuação: ${score}\n Você perdeu o jogo!\n Clique em OK para iniciar um novo jogo`);
+    order = [];
+    clickedOrder = [];
+
+    playGame();
+}
+
+let playGame = () =>{
+    alert('Bem Vindo ao Genius! Iniciando novo jogo!');
+    score = 0;
+    
+    nextLevel();
+}
+
+green.addEventListener('click',click(0));
+red.addEventListener('click',click(1));
+yellow.addEventListener('click',click(2));
+blue.addEventListener('click',click(3));
+
+playGame();
